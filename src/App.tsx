eb3f7111eef5cbaab2af1cfa5ecb5db15004742b
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-09 12:54:29 +0900

    react-routerを使って簡単なroutingを設定した
    とりあえずTop componentを表示出来るようにしてみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+
 import Top from './components/Top';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="container">
-      <Top />
+      <Switch>
+        <Route path="/" component={Top} />
+        <Redirect to="/" />;
+      </Switch>
     </div>
   );
 };
