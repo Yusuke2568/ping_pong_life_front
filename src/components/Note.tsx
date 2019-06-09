ba8caeafa06c96fc0941109af989501f127d97f9
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-09 16:03:31 +0900

    Note componentを作成し、Topで表示するようにした
    material-uiのTextfieldを使用して、textfieldを作成した
    また、local stateを追加して使用できるようにした

---
+++ src/components/Note.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+
+const Note: FC = () => {
+  const [input, setInput] = useState('');
+
+  const changeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <>
+      <TextField
+        id="outlined-full-width"
+        label="content"
+        style={{ margin: 8 }}
+        placeholder="Placeholder"
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={input}
+        onChange={changeInput}
+      />
+    </>
+  );
+};
+
+export default Note;
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 
 import Header from './Header';
+import Note from './Note';
 
 const Top: FC = () => (
   <>
     <Header />
+    <Note />
   </>
 );
 
