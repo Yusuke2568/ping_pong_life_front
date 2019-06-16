75b7897f35eeed38578ab96508a8e60b7713782b
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-16 15:13:23 +0900

    wip

--- src/containers/Note.tsx
+++ src/containers/Note.tsx
@@ -1,9 +1,21 @@
-import { connect } from 'react-redux';
+import React, { FC } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
 
 import { change } from '../actions/note';
 import Note from '../components/Note';
 import { NoteState } from '../reducer';
+
+const noteSelector = (state: NoteState) => state.input;
+
+const _Note = () => {
+  const dispatch = useDispatch();
+  const input = useSelector(noteSelector);
+
+  return (
+    <Note input={input} change={(text: string) => dispatch(change(text))} />
+  );
+};
 
 interface StateProps {
   input: string;
@@ -18,10 +30,11 @@ const mapStateToProps = (state: NoteState): StateProps => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  // ここのchange(text)はactions/noteから持ってきている
   change: text => dispatch(change(text)),
 });
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(Note);
+)(_Note);
