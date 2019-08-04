70cfa01ff3e9b527c021e99ead220b6f42f56481
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-08-04 19:09:58 +0900

    input画面とpreviewをcontainerでwrapし、flexで横並びにする

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+// style
+import styled from 'styled-components';
 import Header from './Header';
 import Note from '../containers/Note';
 import TextPreview from '../containers/TextPreview';
@@ -7,9 +9,15 @@ import TextPreview from '../containers/TextPreview';
 const Top: FC = () => (
   <>
     <Header />
-    <Note />
-    <TextPreview />
+    <Container>
+      <Note />
+      <TextPreview />
+    </Container>
   </>
 );
+
+const Container = styled.div`
+  display: flex;
+`;
 
 export default Top;
